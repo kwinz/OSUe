a7074c71e547365abbbf5dddcc23b873b71ab5cd
Markus Krainz <ldm@gmx.at>
2018-11-02 19:46:16 +0100

    Everything works

--- ispalindrom.c
+++ ispalindrom.c
@@ -1,9 +1,10 @@
 #include "ispalindrom.h"
 
 #include <assert.h>
+#include <ctype.h>
 #include <errno.h>
 #include <stddef.h>
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -32,57 +33,100 @@ int main(int argc, char *argv[]) {
         outfile_arg = optarg;
       } break;
       case '?': {
-        fprintf(stderr, "[%s] ERROR unknown option or missing argument \n", argv[0]);
+        fprintf(stderr, "[%s, %s, %d] ERROR unknown option or missing argument \n", argv[0],
+                __FILE__, __LINE__);
         exit(EXIT_FAILURE);
       } break;
       default:
-        assert(0 && "[%s] We should never reach this if the optstring is valid");
+        assert(0 && "We should never reach this if the optstring is valid");
       }
     }
 
     if (ignorewhitespace_count > 1) {
-      fprintf(stderr, "[%s] ERROR Provide at most one '-s' argument \n", argv[0]);
+      fprintf(stderr, "[%s, %s, %d]  ERROR Provide at most one '-s' argument \n", argv[0],
+              __FILE__, __LINE__);
       exit(EXIT_FAILURE);
     }
 
     if (ignorecase_count > 1) {
-      fprintf(stderr, "[%s] ERROR Provide at most one '-i' argument \n", argv[0]);
+      fprintf(stderr, "[%s, %s, %d]  ERROR Provide at most one '-i' argument \n", argv[0],
+              __FILE__, __LINE__);
       exit(EXIT_FAILURE);
     }
 
     if (o_count > 1) {
-      fprintf(stderr, "[%s] ERROR Provide at most one '-o' argument \n", argv[0]);
+      fprintf(stderr, "[%s, %s, %d]  ERROR Provide at most one '-o' argument \n", argv[0],
+              __FILE__, __LINE__);
       exit(EXIT_FAILURE);
     }
+  }
 
-    // strerror(errno)
-    // fprintf(stderr, "[%s] ERROR errmsg: \r\n", argv[0]);
+  // printf("%d\r\n", number_of_file_args);
+  // fflush(stdout);
+
+  FILE *out_file = NULL;
+
+  if (o_count > 0) {
+    out_file = fopen(outfile_arg, "w");
+    if (out_file == NULL) {
+      fprintf(stderr, "[%s, %s, %d]  ERROR fopen failed on outfile: %s\n", argv[0], __FILE__,
+              __LINE__, strerror(errno));
+      exit(EXIT_FAILURE);
+    }
   }
 
   const int number_of_file_args = argc - optind;
-  printf("%d\r\n", number_of_file_args);
-  fflush(stdout);
 
-  FILE *input_file = fopen("file1.txt", "r");
+  if (number_of_file_args == 0) {
+    handleFile(NULL, out_file, ignorecase_count, ignorewhitespace_count);
+  } else {
+    for (int i = 0; i < number_of_file_args; ++i) {
+      FILE *input_file = fopen(argv[optind + i], "r");
 
-  if (input_file == NULL) {
+      if (input_file == NULL) {
+        if (out_file != NULL) {
+          fclose(out_file);
+        }
+        fprintf(stderr, "[%s,%s, %d] ERROR fopen failed,: %s\n", argv[0], __FILE__, __LINE__,
+                strerror(errno));
+        exit(EXIT_FAILURE);
+      }
 
-    fprintf(stderr, "fopen failed,: %s\n", strerror(errno));
-    exit(EXIT_FAILURE);
+      handleFile(input_file, out_file, ignorecase_count, ignorewhitespace_count);
+      fclose(input_file);
+    }
   }
+
+  if (out_file != NULL) {
+    fclose(out_file);
+  }
+
+  return EXIT_SUCCESS;
+}
+
+void handleFile(FILE *input_file, FILE *out_file, int8_t ignore_case, int8_t ignore_whitespace) {
 
   // warning: linebuffer_size is not const, as getline might modify it when it
   // needs to resize the buffer
-  int linebuffer_size = 2;
+  size_t linebuffer_size = 2;
   char *line = malloc(linebuffer_size);
 
   if (line == NULL) {
-    assert(0 && "malloc failed");
+    if (input_file != NULL) {
+      fclose(input_file);
+    }
+    if (out_file != NULL) {
+      fclose(out_file);
+    }
+
+    fprintf(stderr, "FATAL ERROR out of memory");
+    exit(EXIT_FAILURE);
   }
 
   // getline() returns -1 on failure to read a line (including EOF).
   ssize_t characters_read;
-  while ((characters_read = getline(&line, &linebuffer_size, input_file)) != -1) {
+  while ((characters_read =
+              getline(&line, &linebuffer_size, input_file == NULL ? stdin : input_file)) != -1) {
     // remove tailing newline (which is also the first occurance of '\n' because
     // we read line by line)
     {
@@ -92,24 +136,24 @@ int main(int argc, char *argv[]) {
 
     // check if string is palindrome and print the result
     {
-      const uint8_t is_palindrom = isPalindrom(line, ignorecase_count, ignorewhitespace_count);
-      printf("%s %s \n", line, is_palindrom ? "is a palindrom" : "is not a palindrom");
+      const uint8_t is_palindrom = isPalindrom(line, ignore_case, ignore_whitespace);
+      fprintf(out_file == NULL ? stdout : out_file, "%s %s \n", line,
+              is_palindrom ? "is a palindrom" : "is not a palindrom");
     }
   }
 
-  puts("closing");
-
-  fclose(input_file);
-
-  return EXIT_SUCCESS;
+  free(line);
 }
 
 /**
  * @brief returns != 0 if the string is a palindrom
  *
  * @param c_string pointer to '\0' terminated char sequence which will be tested
- * for being a palisndrom
- * @param
+ * for being a palindrom
+ * @param ignore_case if != 0 then then upper/lower-case is ignored when processing
+ * palindrome
+ * @param ignore_whitespace if != 0 then then all whitespace (not including special
+ * characters like '\t') is ignored when processing palindrome
  */
 int8_t isPalindrom(char *c_string, int8_t ignore_case, int8_t ignore_whitespace) {
 

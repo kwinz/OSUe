04f78b1c08bd015f13e2cc24a3a1d4344bf40a2f
Markus Krainz <ldm@gmx.at>
2018-11-10 22:35:56 +0100

    Can parse port + some reading from client

--- ue1b/server.c
+++ ue1b/server.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
+#include <limits.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -68,10 +69,28 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  struct sockaddr_in sa;
+  char *endpointer;
+  long port = strtol(port_string, &endpointer, 0);
+  char *endpointer2 = port_string + strlen(port_string);
 
+  fprintf(stderr, "[%s, %s, %d]  Pointer1 %p \n", argv[0], __FILE__, __LINE__, endpointer);
+  fprintf(stderr, "[%s, %s, %d]  Pointer2 %p \n", argv[0], __FILE__, __LINE__, endpointer2);
+
+  //FIXME: set narrower limits
+  if (port == LONG_MIN || port == LONG_MAX || (endpointer != endpointer2)) {
+
+    fprintf(stderr, "[%s, %s, %d]  ERROR Could not parse port. \n", argv[0], __FILE__, __LINE__);
+
+    // note: strtol only sets errno, if the long range was exceeded, but not in case of other
+    // conversion errors.
+    exit(EXIT_FAILURE);
+  }
+
+  fprintf(stderr, "[%s, %s, %d]  Port is %ld\n", argv[0], __FILE__, __LINE__, port);
+
+  struct sockaddr_in sa;
   sa.sin_family = AF_INET;
-  sa.sin_port = htons(8080);
+  sa.sin_port = htons(port);
   memset(&(sa.sin_addr), 0, sizeof sa.sin_addr);
   // inet_aton("63.161.169.137", sa.sin_addr.s_addr);
 
@@ -89,9 +108,11 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
+  fprintf(stderr, "[%s, %s, %d]  Waiting for incoming clients. \n", argv[0], __FILE__, __LINE__);
+
   int connfd = accept(sockfd, NULL, NULL);
   if (connfd < 0) {
-    fprintf(stderr, "[%s, %s, %d]  ERROR Error while accepting incomming request. \n", argv[0],
+    fprintf(stderr, "[%s, %s, %d]  ERROR Error while accepting incomming request. %s \n", argv[0],
             __FILE__, __LINE__, strerror(errno));
     exit(EXIT_FAILURE);
   }
@@ -99,10 +120,24 @@ int main(int argc, char *argv[]) {
   FILE *sockfile = fdopen(sockfd, "r+");
 
   if (sockfile == NULL) {
-    fprintf(stderr, "[%s, %s, %d]  ERROR Could not open stream with incoming connection. \n",
+    fprintf(stderr, "[%s, %s, %d]  ERROR Could not open stream with incoming connection. %s \n",
             argv[0], __FILE__, __LINE__, strerror(errno));
     exit(EXIT_FAILURE);
   }
 
+  fprintf(stderr, "[%s, %s, %d]  Got client. Reading.. \n", argv[0], __FILE__, __LINE__);
+
+  char buf[1024];
+
+  while (fgets(buf, sizeof(buf), sockfile) != NULL) {
+
+    fprintf(stderr, "[%s, %s, %d] Read %s \n", argv[0], __FILE__, __LINE__, buf);
+
+    char *token = strtok(buf, " ");
+    while (token != NULL) {
+      fprintf(stderr, "token: %s\n", token);
+      token = strtok(NULL, " ");
+    }
+  }
   return EXIT_SUCCESS;
 }

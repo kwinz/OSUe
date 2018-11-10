1ce81c77a8d877d71003152c5431987c391cb002
Markus Krainz <ldm@gmx.at>
2018-11-10 21:13:27 +0100

    Begun server

--- ue1b/server.c
+++ ue1b/server.c
@@ -1,1 +1,71 @@
-int main(int argc, char *argv[]) {}
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+
+  // parse arguments
+  char *doc_root = NULL;
+  char *port_string = "8080", *indexfile_string = "index.html";
+  int port_count = 0, indexfile_count = 0;
+
+  {
+    const char *optstring = "p:i:";
+    int c;
+
+    // getopt returns -1 if there is no more character
+    // Or it returns '?' in case of unknown option or missing option argument
+    while ((c = getopt(argc, argv, optstring)) != -1) {
+      switch (c) {
+      case 'p': {
+        ++port_count;
+        port_string = optarg;
+      } break;
+      case 'i': {
+        ++indexfile_count;
+        indexfile_string = optarg;
+      } break;
+      case '?': {
+        fprintf(stderr, "[%s, %s, %d] ERROR unknown option or missing argument \n", argv[0],
+                __FILE__, __LINE__);
+        exit(EXIT_FAILURE);
+      } break;
+      default:
+        assert(0 && "We should never reach this if the optstring is valid");
+      }
+    }
+
+    if (port_count > 1) {
+      fprintf(stderr, "[%s, %s, %d]  ERROR Provide at most one '-p' argument \n", argv[0],
+              __FILE__, __LINE__);
+      exit(EXIT_FAILURE);
+    }
+
+    if (indexfile_count > 1) {
+      fprintf(stderr, "[%s, %s, %d]  ERROR Provide at most one '-o' argument \n", argv[0],
+              __FILE__, __LINE__);
+      exit(EXIT_FAILURE);
+    }
+
+    const int positional_args_count = argc - optind;
+  }
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+  }
+
+  // error
+
+  return EXIT_SUCCESS;
+}

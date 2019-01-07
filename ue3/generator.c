d40c380bedafd54e00c89b5962c60f64dd69611b
Markus Krainz <ldm@gmx.at>
2019-01-07 23:40:17 +0100

    Can shuffle vertices

--- ue3/generator.c
+++ ue3/generator.c
@@ -1,7 +1,9 @@
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // Choose a limit on the maximum number
 // of edges which we can process.
@@ -22,8 +24,15 @@ typedef struct Edge {
 int main(int argc, char *argv[]) {
   // C99 VLA
   Edge_t graph[argc - 1];
-  fprintf(stderr, "arguments %d\n", argc);
+  fprintf(stderr, "edges: %d\n", argc - 1);
 
+  if (argc == 1) {
+    fprintf(stderr, "Provide at least one edge. \n");
+    return EXIT_FAILURE;
+  }
+
+  // parse edges from arguments
+  int max_vert = 0;
   for (int i = 0; i < argc - 1; i++) {
     char *a_str = strtok(argv[i + 1], "-");
     if (a_str == NULL) {
@@ -51,7 +60,41 @@ int main(int argc, char *argv[]) {
 
     graph[i].a = a;
     graph[i].b = b;
+
+    max_vert = fmax(a, max_vert);
+    max_vert = fmax(b, max_vert);
   }
+
+  fprintf(stderr, "vertices: %d\n", max_vert + 1);
+
+  // create array of vertices
+  int vertices[max_vert + 1];
+  for (int i = 0; i <= max_vert; ++i) {
+    vertices[i] = i;
+  }
+
+  for (int i = 0; i <= max_vert; ++i) {
+    fprintf(stderr, "%d,", vertices[i]);
+  }
+
+  fprintf(stderr, "\nBEFORE SHUFFLE\n");
+
+  // shuffle
+  srand(time(NULL));
+  for (int i = 0; i < max_vert - 1; ++i) {
+    const int j = i + rand() % (max_vert + 1 - i);
+    fprintf(stderr, "swapping indizes: %d,%d\n", i, j);
+    const int temp = vertices[i];
+    vertices[i] = vertices[j];
+    vertices[j] = temp;
+
+    for (int i = 0; i <= max_vert; ++i) {
+      fprintf(stderr, "%d,", vertices[i]);
+    }
+    fprintf(stderr, "\n----\n");
+  }
+
+  fprintf(stderr, "AFTER SHUFFLE\n");
 
   return EXIT_SUCCESS;
 }

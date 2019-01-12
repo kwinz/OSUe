df06553456e65f489389a7f86effec659455d869
Markus Krainz <ldm@gmx.at>
2019-01-12 22:24:18 +0100

    Add shm ringbuffer sample code

--- ue3/supervisor.c
+++ ue3/supervisor.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -11,6 +12,36 @@ struct myshm {
   unsigned int state;
   unsigned int data[MAX_DATA];
 };
+
+// taken heavy inspiration from "Exercise 3: Shared Memory [..]" slides Platzer (2018)
+
+#define BUF_LEN 8
+// points to shared memory mapped with mmap(2)
+int *buf;
+// tracks free space, initialized to BUF_LEN
+sem_t *free_sem;
+// tracks used space, initialized to 0
+sem_t *used_sem;
+int write_pos = 0;
+void circ_buf_write(int val) {
+  // writing requires free space
+  sem_wait(free_sem);
+  buf[write_pos] = val;
+  // space is used by written data
+  sem_post(used_sem);
+  write_pos = (write_pos + 1) % BUF_LEN;
+}
+
+int read_pos = 0;
+int circ_buf_read() {
+  // reading requires data (used space)
+  sem_wait(used_sem);
+  int val = buf[read_pos];
+  // reading frees up space
+  sem_post(free_sem);
+  read_pos = (read_pos + 1) % BUF_LEN;
+  return val;
+}
 
 int main(int argc, char *argv[]) {
 
